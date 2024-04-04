6b2133894be8d516be922fdd0266b429f434703c
Shivanshu Bansal <shivanshu_bansal@sfu.ca>
2024-04-04 13:34:41 -0700

    add main script to run setup

---
+++ main.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Check if script is run with sudo
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run with sudo."
+    exit 1
+fi
+
+echo "alias ll='ls -la'" >> ~/.bashrc
+echo "alias public='cd /mnt/remote/'" >> ~/.bashrc
+
+# Step 1: Enable Networking
+cat << 'EOF' > internetToTarget.sh
+
+#!/bin/sh
+sudo route add default gw 192.168.7.1
+echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf
+
+EOF
+
+chmod +x internetToTarget.sh
+
+echo "./internetToTarget.sh" >> ~/.bashrc
+source ~/.bashrc
+
+# Step 2: Updates
+sudo apt update
+sudo apt upgrade
+sudo apt install nfs-common
+
+# Step 3: Enable NFS
+sudo mkdir -p /mnt/remote
+sudo chown $USER /mnt/remote
+
+cat << 'EOF' > mountNFS.sh
+
+#!/bash/sh
+sudo mount -t nfs 192.168.7.1:home/shivanshu/cmpt433/public /mnt/remote
+
+EOF
+
+chmod +x mountNFS.sh
+
+echo "./mountNFS.sh" >> ~/.bashrc
+source ~/.bashrc
